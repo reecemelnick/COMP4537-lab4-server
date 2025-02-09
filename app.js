4c1fe4691b299408b4fade052523fc4739b9cb31
Reece Melnick <reececianmelnick@gmail.com>
2025-02-08 20:36:53 -0800

    d

--- app.js
+++ app.js
@@ -1,163 +1,166 @@
 const http = require('http');
 let url = require('url');
 const dt = require('./modules/utils');
+const userMessages = require('./lang/en/en.js');
 
-let dictionary = [];
-let currentReq = 0;
+class Server {
 
-http.createServer(function (req, res) {
+  constructor() {
+    this.dictionary = [];
+    this.currentReq = 0; 
+  }
 
-  console.log("The server received a request");
 
-  if (req.method === "OPTIONS") {
-    res.writeHead(204, {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST, GET",
-        "Access-Control-Allow-Headers": "Content-Type"
-    });
-    return res.end();
-}
-
-    if (req.url === '/') {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Server is running...");
-      return;
-    }
-
-    if (req.url === '/favicon.ico') {
-      res.writeHead(204);
-      res.end();
-      return;
-    }
-
-    console.log("Method:", req.method);
-console.log("URL:", req.url);
-
-  if(req.method === "POST" && req.url === '/api/definitions/') {
-      
-      console.log("in post request");
+  handlePost(req, res) {
+    this.currentReq++;
     
-      currentReq++;
+          let body = "";
+    
+          // on data, build body by chunks
+          req.on("data", (chunk) => {
+            body += chunk;
+          });
+    
+          // when incoming data is done, parse body
+          req.on("end", () => {
+          
+            try {
+                let data = JSON.parse(body);
+                let word = data.word;
+                let definition = data.definition;
 
-      let body = "";
+                console.log(data);
+    
+                let valid = this.validateInput(word);
+                if(!valid) {
+                  res.writeHead(400, { "Content-Type": "text/plain", "Access-Control-Allow-Origin": "*" });
+                  res.end(userMessages.invalidWord);
+                  return;
+                } 
+    
+                valid = this.validateInput(definition);
+                if(!valid) {
+                  res.writeHead(400, { "Content-Type": "text/plain", "Access-Control-Allow-Origin": "*" });
+                  res.end(userMessages.invalidDef);
+                  return;
+                } 
+    
+                let added = this.addToDict(data);
+    
+                console.log(added);
+    
+                console.log(this.dictionary);
+    
+                res.writeHead(200, { "Content-Type": "text/html", "Access-Control-Allow-Origin": "*" });
+                res.end(added);
+            } catch (error) {
+              console.error("Error parsing JSON or processing data:", error);
+                res.writeHead(400, { "Content-Type": "text/plain", "Access-Control-Allow-Origin": "*" });
+                res.end(userMessages.invalidJSON);
+            }
+        });
+  }
 
-      // on data, build body by chunks
-      req.on("data", function(chunk) {
-        body += chunk;
-      });
-
-      // when incoming data is done, parse body
-      req.on("end", () => {
-        try {
-            let data = JSON.parse(body);
-            let word = data.word || "Unknown";
-            let definition = data.definition || "unknown";
-
-            let valid = validateInput(word);
-            if(!valid) {
-              res.writeHead(400, { "Content-Type": "text/plain", "Access-Control-Allow-Origin": "*" });
-              res.end("Invalid input");
-            } 
-
-            valid = validateInput(definition);
-            if(!valid) {
-              res.writeHead(400, { "Content-Type": "text/plain", "Access-Control-Allow-Origin": "*" });
-              res.end("Invalid input");
-              return;
-            } 
-
-            let added = addToDict(data);
-
-            console.log(added);
-            console.log(dictionary);
-
-            res.writeHead(200, { "Content-Type": "text/html", "Access-Control-Allow-Origin": "*" });
-            res.end(added);
-        } catch (error) {
+  handleGet(req, res) {
+    this.currentReq++;
+        
+          let q = url.parse(req.url, true);
+          let qdata = q.query;
+          console.log(qdata.word);
+    
+          let valid = this.validateInput(qdata.word);
+          if(!valid) {
             res.writeHead(400, { "Content-Type": "text/plain", "Access-Control-Allow-Origin": "*" });
-            res.end("Invalid JSON");
-        }
-    });
-  }
-
-  if(req.method === "GET" && req.url.startsWith('/api/definitions/')) {
-
-    currentReq++;
-  
-    let q = url.parse(req.url, true);
-    let qdata = q.query;
-    console.log(qdata.word);
-
-    let valid = validateInput(qdata.word);
-    if(!valid) {
-      res.writeHead(400, { "Content-Type": "text/plain", "Access-Control-Allow-Origin": "*" });
-      res.end("Invalid input");
-      return;
-    }
-
-    let desc = checkDict(qdata.word);
-
-    res.writeHead(200, { "Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"});
-    res.end(desc);
+            res.end(userMessages.invalidWord);
+            return;
+          }
     
+          let desc = this.checkDict(qdata.word);
+          console.log(desc);
+    
+          res.writeHead(200, { "Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"});
+          res.end(desc);
   }
 
 
-  // if (req.url === '/api/definitions/') {
-  //   res.writeHead(200, { "Content-Type": "text/plain" });
-  //   res.end("Server is running...");
-  //   return;
-  // }
+  start() {
+    http.createServer((req, res) => {
 
-}).listen(process.env.PORT || 8888);
-
-
-let addToDict = (word) => {
-  for(let i = 0; i < dictionary.length; i++) {
-    if(dictionary[i].word === word.word) {
-      dictionary[i].definition = word.definition;
-      return "Word Already Exists. Definition changed.";
+      console.log("The server received a request");
+    
+      if (req.method === "OPTIONS") {
+        res.writeHead(204, {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type"
+        });
+        return res.end();
     }
+        console.log("Method:", req.method);
+        console.log("URL:", req.url);
+    
+      if(req.method === "POST" && req.url === '/api/definitions/') {
+          this.handlePost(req, res);
+          
+      } else if(req.method === "GET" && req.url.startsWith('/api/definitions/')) {
+          this.handleGet(req, res);
+          
+      } else {
+          res.writeHead(404, { "Content-Type": "text/plain", "Access-Control-Allow-Origin": "*" });
+          res.end(userMessages.pageNotFound);
+      }
+    
+    
+    }).listen(8888);
   }
-  dictionary.push(word);
-  let responseMessage = `Request # ${currentReq} updated on ${dt.getDate()}
-  <br><br>Words in dictionary ${dictionary.length}
-  <br><br>New entry recorded:
-  <br><br>"${word.word} : ${word.definition}"`;
-  return responseMessage;
-}
 
-let checkDict = (word) => {
-  for(let i = 0; i < dictionary.length; i++) {
-    if(dictionary[i].word === word) {
-      return dictionary[i].definition;
+
+  addToDict = (word) => {
+    for(let i = 0; i < this.dictionary.length; i++) {
+      if(this.dictionary[i].word === word.word) {
+        this.dictionary[i].definition = word.definition;
+        return userMessages.wordExists;
+      }
     }
-  }
-  let responseMessage = `Request # ${currentReq}, word '${word}' not found!`;
-  return responseMessage;
-}
-
-let validateInput = (str) => {
-
-  if(!str) {
-    return false;
-  }
-
-  let numbers = "0123456789";
-
-  if(str.length === 0) {
-      return false;
+    this.dictionary.push(word);
+    return userMessages.entryAdded(this.currentReq, dt.getDate(), this.dictionary.length, word.word, word.definition);
   }
   
-  if(str.trim().length === 0) {
-      return false;
-  }
-
-  for(let i = 0; i < str.length; i++) {
-      if(numbers.indexOf(str[i]) !== -1) {
-          return false;
+  checkDict = (word) => {
+    for(let i = 0; i < this.dictionary.length; i++) {
+      if(this.dictionary[i].word === word) {
+        return this.dictionary[i].definition;
       }
+    }
+    return userMessages.wordNotFound(this.currentReq, word);
+  }
+  
+  validateInput = (str) => {
+  
+    if(!str) {
+      return false;
+    }
+  
+    let numbers = "0123456789";
+  
+    if(str.length === 0) {
+        return false;
+    }
+    
+    if(str.trim().length === 0) {
+        return false;
+    }
+  
+    for(let i = 0; i < str.length; i++) {
+        if(numbers.indexOf(str[i]) !== -1) {
+            return false;
+        }
+    }
+  
+    return true;
   }
 
-  return true;
 }
+
+let app = new Server();
+app.start();
